f0477996825166d3faf5612966a2ad2100f57012
SushmaGowda7 <110340352+SushmaGowda7@users.noreply.github.com>
2023-01-04 14:46:48 +0530

    request/response updated

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('Sushma Gowda');
+    const url = req.url;
+    const method = req.method;
+    
+    if(url === '/home'){
+        res.write('<html>');
+        res.write('<head><title>My Page</title></head>');
+        res.write('<body><h1>Welcome home</h1></body>');
+        res.write('<html>');
+        return res.end();
+
+    } else if(url === '/about'){
+        res.write('<html>');
+        res.write('<head><title>My Page</title></head>');
+        res.write('<body><h1>Welcome About us page</h1></body>');
+        res.write('<html>');
+        return res.end();
+
+    } else if(url === '/node'){
+        res.write('<html>');
+        res.write('<head><title>My Page</title></head>');
+        res.write('<body><h1>Hello from my Node.js page</h1></body>');
+        res.write('<html>');
+        return res.end();
+    }
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My Page</title></head>');
+    res.write('<body><h1>Hello from my Node.js Project!</h1></body>');
+    res.write('<html>');
+    res.end();
 })
 
 server.listen(4000);
